444d470626a1bf90765cc0338e7caee226643faf
David Leuliette <dleuliette@gmail.com>
2020-05-09 11:01:19 +0200

    Fix issues with RichText

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
-import { RichText } from "prismic-reactjs"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
