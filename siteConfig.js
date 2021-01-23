661c79c0965c37d3e35901c173629be741d9536d
David Leuliette <dleuliette@gmail.com>
2021-01-23 22:24:51 +0100

    Update about page

--- siteConfig.js
+++ siteConfig.js
@@ -1,10 +1,17 @@
 module.exports = {
   name: "ART au féminin",
+  title: "ART au féminin",
   shortName: "ART",
   description: "Explorons ensemble l’histoire des femmes artistes",
-  url: "https://artaufeminin.fr", // Domain of your site without prefix!
+  url: "https://artaufeminin.fr",
+  siteUrl: "https://artaufeminin.fr",
   prefix: "/",
   author: "Aldjia",
-  instagram: "artaufeminin",
+  email: "artaufemininlepodcast@gmail.com",
+  social: {
+    instagram: "artaufeminin",
+    instagramUrl: "https://instagram.com/artaufeminin",
+    facebookUrl: "https://www.facebook.com/podcastart",
+  },
   anchorRssUrl: "https://anchor.fm/s/9483df8/podcast/rss",
 }
