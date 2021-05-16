32005ecf71a5c4e138675b23c1a0764a01be7b51
David Leuliette <dleuliette@gmail.com>
2021-05-16 16:01:42 +0200

    bump react-helmet

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Card({ title, children }: Props): ReactElement {
   return (
     <div className="bg-white shadow-md mb-12">
-      <div className="p-6">
+      <div className="p-8">
         <h2 className="text-4xl text-gray-500 font-bold mt-0 mb-0">{title}</h2>
       </div>
       {children}
