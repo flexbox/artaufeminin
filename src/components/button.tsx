4abca35145a15b0a1207ce9dbf57f938984cd81e
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-22 16:59:41 +0200

    storybook template buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,22 +2,21 @@ import React, { ReactElement } from "react"
 
 interface ButtonProps {
   children: any
-  variant: "ghost" | "outline" | "solid"
+  variant: "ghost" | "solid"
 }
 
 export default function Button({ variant, children }: ButtonProps) {
   const classNamesDefault = "py-2 px-4  font-semibold rounded-lg shadow-md"
 
-  let classNamesVariant =
-    "bg-indigo-500 text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75 font-merri "
+  let classNamesVariant = " text-white   font-merri m-4 text-xs"
   if (variant === "ghost") {
-    classNamesVariant = "text-blue-500 font-merri"
+    classNamesVariant =
+      "text-blue-500 font-merri border-blue-500 border-2 border-solid m-4 hover:border-blue-600 hover:text-blue-600"
   }
-  if (variant === "outline") {
-    classNamesVariant = "text-blue-500"
-  }
+
   if (variant === "solid") {
-    classNamesVariant = "bg-blue-500 text-white font-merri"
+    classNamesVariant =
+      "bg-blue-500 text-white font-merri text-md m-4 p-4 hover:bg-blue-600"
   }
 
   return (
