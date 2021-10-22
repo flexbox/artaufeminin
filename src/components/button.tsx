5ff8da4cc5861be89ea604636e7061629a978ba6
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-22 18:03:44 +0200

    add css

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,19 +2,22 @@ import React, { ReactElement } from "react"
 
 interface ButtonProps {
   children: any
-  variant: "outline" | "solid" | "large"
+  variant?: "outline" | "solid" | "ghost"
   as?: string
+  size?: string
 }
 
 export default function Button({
-  variant,
+  variant = "solid",
   children,
+  size,
   as,
   ...props
 }: ButtonProps) {
-  const classNamesDefault = "py-2 px-4 my-4  font-semibold rounded-lg shadow-md"
+  const classNamesDefault = "py-2 px-4 my-4 font-semibold rounded-lg"
 
   let classNamesVariant = " text-white   font-merri m-4 text-xs"
+  let classNamesSize = ""
   if (variant === "outline") {
     classNamesVariant =
       " text-blue-500 font-merri border-blue-500 border-2 border-solid m-0 hover:border-blue-600 hover:text-blue-600"
@@ -24,9 +27,18 @@ export default function Button({
     classNamesVariant =
       "  bg-blue-500 text-white font-merri text-md m-0  hover:bg-blue-600"
   }
-  if (variant === "large") {
+  if (variant === "ghost") {
     classNamesVariant =
       "bg-blue-500 text-white font-merri text-md hover:bg-blue-600 py-4 px-8"
+  }
+  if (size === "s") {
+    classNamesSize = "py-12 px-12"
+  }
+  if (size === "m") {
+    classNamesSize = "py-24 px-24"
+  }
+  if (size === "l") {
+    classNamesSize = "py-48 px-48"
   }
   if (as === "a") {
     return (
@@ -36,7 +48,9 @@ export default function Button({
     )
   }
   return (
-    <button className={`${classNamesDefault} ${classNamesVariant}`}>
+    <button
+      className={`${classNamesDefault} ${classNamesVariant} ${classNamesSize}`}
+    >
       {children}
     </button>
   )
