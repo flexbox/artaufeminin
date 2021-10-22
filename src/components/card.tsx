5d5aa2f674713ab52bfe2767a1d6aa7c26260f5c
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-22 17:41:10 +0200

    add card css

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react"
+import Button from "./button"
 
 interface Props {
   title: string
@@ -7,11 +8,14 @@ interface Props {
 
 export default function Card({ title, children }: Props): ReactElement {
   return (
-    <div className="bg-white shadow-md mb-12">
-      <div className="p-8">
+    <div className="bg-white shadow-md m-12 p-8">
+      <div className="mb-6">
         <h2 className="text-4xl text-gray-500 font-bold mt-0 mb-0">{title}</h2>
       </div>
       {children}
+      <div>
+        <Button variant="outline">Default</Button>
+      </div>
     </div>
   )
 }
