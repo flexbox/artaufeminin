8f0be537dfe183371eab52f54634c1c8aa4546d5
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-22 18:03:44 +0200

    add css

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -17,13 +17,26 @@ export default {
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="solid">Primary</Button>
+  return (
+    <>
+      <Button>Hello</Button>
+      <Button variant="outline">Hello</Button>
+      <Button variant="solid">Hello</Button>
+      <Button variant="ghost">Hello</Button>
+    </>
+  )
 }
 const TemplateDefault: ComponentStory<typeof Button> = (args) => {
   return <Button variant="outline">Default</Button>
 }
-const TemplateLarge: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="large">Large</Button>
+const TemplateSizes: ComponentStory<typeof Button> = (args) => {
+  return (
+    <>
+      <Button size="s">Small</Button>
+      <Button size="m">Medium</Button>
+      <Button size="l">Large</Button>
+    </>
+  )
 }
 const TemplateDefaultGhost: ComponentStory<typeof Button> = (args) => {
   return <Button variant="outline">Default</Button>
@@ -31,25 +44,8 @@ const TemplateDefaultGhost: ComponentStory<typeof Button> = (args) => {
 const TemplateSmallGhost: ComponentStory<typeof Button> = (args) => {
   return <Button variant="outline">Small</Button>
 }
-const TemplateFit: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="solid">Fit</Button>
-}
-const TemplateFitGhost: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="outline">Fit</Button>
-}
-const TemplateFitSmall: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="outline">Fit + Small</Button>
-}
-const TemplateFitSmallGhost: ComponentStory<typeof Button> = (args) => {
-  return <Button variant="outline">Fit + Small</Button>
-}
 
 export const Primary = Template.bind({})
 export const Default = TemplateDefault.bind({})
-export const Large = TemplateLarge.bind({})
-export const DefaultGhost = TemplateDefaultGhost.bind({})
+export const Large = TemplateSizes.bind({})
 export const SmallGhost = TemplateSmallGhost.bind({})
-export const Fit = TemplateFit.bind({})
-export const FitGhost = TemplateFitGhost.bind({})
-export const FitSmall = TemplateFitSmall.bind({})
-export const FitSmallGhost = TemplateFitSmallGhost.bind({})
--- src/stories/Newsletter.Stories.tsx
+++ src/stories/Newsletter.Stories.tsx
@@ -1,20 +1,18 @@
 import React from "react"
 import { ComponentStory, ComponentMeta } from "@storybook/react"
-import Card from "../components/card"
+import Newsletter from "../components/newsletter"
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Card",
-  component: Card,
+  title: "Example/Newsletter",
+  component: Newsletter,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: "color" },
   },
-} as ComponentMeta<typeof Card>
+} as ComponentMeta<typeof Newsletter>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Card> = (args) => {
-  return <Card title="Je suis un titre">HEY</Card>
-}
+const Template: ComponentStory<typeof Newsletter> = (args) => <Newsletter />
 
 export const Primary = Template.bind({})
