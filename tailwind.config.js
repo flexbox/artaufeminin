21bde5e16acd2ac78d1f00f1deea0f3e0f64c431
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-10-22 14:55:04 +0200

    add font to storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        merri: "'Merriweather', serif",
+      },
+    },
   },
   variants: {},
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
