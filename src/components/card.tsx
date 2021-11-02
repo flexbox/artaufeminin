befa772af89f1a4cecd733ba6ea1f1d2a06ebda4
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-02 17:01:59 +0100

    margin bot card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Card({ title, children }: Props): ReactElement {
   return (
-    <div className="bg-white shadow-md p-8">
+    <div className="bg-white shadow-md p-8 mb-4">
       <div className="mb-6">
         <h2 className="text-4xl text-gray-500 font-bold mt-0 mb-0">{title}</h2>
       </div>
