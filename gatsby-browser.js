a99ca302612bf8c498afd36596430a380cd49bae
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-04 11:48:25 +0100

    change config for prismic graphql

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,8 +3,8 @@ import "typeface-merriweather"
 
 import "./src/styles/tailwind.css"
 
-// Prismic
-const { registerLinkResolver } = require("@prismicio/gatsby-source-prismic")
+const { registerLinkResolver } = require("gatsby-source-prismic-graphql")
 const { linkResolver } = require("./src/utils/linkResolver")
+// Prismic
 
 registerLinkResolver(linkResolver)
