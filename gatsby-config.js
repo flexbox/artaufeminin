d3fda9f3ee7425772391f65605d63665ac1750a6
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-05 11:53:50 +0100

    add json file

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-image`,
+    `gatsby-source-prismic`,
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
@@ -60,27 +61,46 @@ module.exports = {
     {
       resolve: "gatsby-source-prismic",
       options: {
+        // repositoryName: "artaufeminin",
         repositoryName: "artaufeminin",
+        customTypesApiToken: `${process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN}`,
         linkResolver: (doc) => linkResolver(doc),
-        // required
         schemas: {
-          // pages: [
-          //   {
-          //     // optional
-          //     type: "Blog_post", // TypeName from prismic
-          //     match: "/article/:uid", // pages will be generated under this pattern
-          //     previewPath: "/article", // optional path for unpublished documents
-          //     component: require.resolve("./src/templates/article.tsx"),
-          //     sortBy: "date_ASC", // optional, default: meta_lastPublicationDate_ASC; useful for pagination
-          //   },
-          // ],
-          extraPageFields: "article_type", // optional, extends pages query to pass extra fields
-          sharpKeys: [
-            /image|photo|picture/, // (default)
-            "profilepic",
-          ],
+          blog_post: require("./custom_types/blog_post.json"),
+          faq: require("./custom_types/faq.json"),
         },
+        // required
+        //schemas: {
+
+        // pages: [
+        //   {
+        //     // optional
+        //     type: "Blog_post", // TypeName from prismic
+        //     match: "/article/:uid", // pages will be generated under this pattern
+        //     previewPath: "/article", // optional path for unpublished documents
+        //     component: require.resolve("./src/templates/article.tsx"),
+        //     sortBy: "date_ASC", // optional, default: meta_lastPublicationDate_ASC; useful for pagination
+        //   },
+        // ],
+        // extraPageFields: "article_type", // optional, extends pages query to pass extra fields
+        // sharpKeys: [
+        //   /image|photo|picture/, // (default)
+        //   "profilepic",
+        // ],
       },
     },
+
+    "gatsby-plugin-image",
+    // {
+    //   resolve: "gatsby-source-prismic",
+    //   options: {
+    //     repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+    //     customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+    //     linkResolver: (doc) => linkResolver(doc),
+    //     schemas: {
+    //       page: require("./custom_types/blog_post.json"),
+    //     },
+    //   },
+    // },
   ],
 }
