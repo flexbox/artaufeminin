47b316f0c35ca044c9475f4ecbc35762465f3a93
David Leuliette <dleuliette@gmail.com>
2021-11-18 12:16:30 +0100

    migrate faq to make prismic happy

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,87 +1,87 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { RichText } from "prismic-reactjs"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import { RichText } from "prismic-reactjs"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Author from "../components/author"
-import CustomRichText from "../components/customRichText"
-import { formatHumanDate } from "../utils/date"
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+// import Author from "../components/author"
+// import CustomRichText from "../components/customRichText"
+// import { formatHumanDate } from "../utils/date"
 
-export default function Article(props) {
-  const doc = props.data.allBlog_posts.edges.slice(0, 1).pop()
+// export default function Article(props) {
+//   const doc = props.data.allBlog_posts.edges.slice(0, 1).pop()
 
-  if (!doc) return null
+//   if (!doc) return null
 
-  const title = RichText.asText(doc.node.title)
-  const description = RichText.asText(doc.node.description)
-  const datePublished = formatHumanDate(doc.node.date)
+//   const title = RichText.asText(doc.node.title)
+//   const description = RichText.asText(doc.node.description)
+//   const datePublished = formatHumanDate(doc.node.date)
 
-  const { image } = doc.node
+//   const { image } = doc.node
 
-  return (
-    <Layout>
-      <SEO title={title} description={description} />
+//   return (
+//     <Layout>
+//       <SEO title={title} description={description} />
 
-      <article className="post-content">
-        <header className="post-content-header">
-          <h1 className="post-content-title">{title}</h1>
-        </header>
+//       <article className="post-content">
+//         <header className="post-content-header">
+//           <h1 className="post-content-title">{title}</h1>
+//         </header>
 
-        <div className="post-content-excerpt">{description}</div>
+//         <div className="post-content-excerpt">{description}</div>
 
-        {image && (
-          <div className="text-center mb-10">
-            <figure className="post-content-figure">
-              <img className="mx-auto" src={image.url} alt={image.alt} />
-              <figcaption className="mt-8">
-                {image.alt} {image.copyright && `© ${image.copyright}`}
-              </figcaption>
-            </figure>
-          </div>
-        )}
+//         {image && (
+//           <div className="text-center mb-10">
+//             <figure className="post-content-figure">
+//               <img className="mx-auto" src={image.url} alt={image.alt} />
+//               <figcaption className="mt-8">
+//                 {image.alt} {image.copyright && `© ${image.copyright}`}
+//               </figcaption>
+//             </figure>
+//           </div>
+//         )}
 
-        <div className="post-content-body">
-          <div className="mb-20">
-            <CustomRichText render={doc.node.content} />
-          </div>
+//         <div className="post-content-body">
+//           <div className="mb-20">
+//             <CustomRichText render={doc.node.content} />
+//           </div>
 
-          <p className="text-gray-500 mb-20">
-            Vous avez aimé cet article ?{" "}
-            <a
-              href="https://fr.tipeee.com/art-au-feminin"
-              title="Sponsoriser les épisodes"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Devenez mécène sur tipeee m’aide beaucoup.
-            </a>
-            <br />
-            <em>Publié {datePublished}</em>
-          </p>
-        </div>
+//           <p className="text-gray-500 mb-20">
+//             Vous avez aimé cet article ?{" "}
+//             <a
+//               href="https://fr.tipeee.com/art-au-feminin"
+//               title="Sponsoriser les épisodes"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               Devenez mécène sur tipeee m’aide beaucoup.
+//             </a>
+//             <br />
+//             <em>Publié {datePublished}</em>
+//           </p>
+//         </div>
 
-        <Author />
-      </article>
-    </Layout>
-  )
-}
+//         <Author />
+//       </article>
+//     </Layout>
+//   )
+// }
 
-export const query = graphql`
-  query ArticleQuery($uid: String) {
-    allBlog_posts(uid: $uid) {
-      edges {
-        node {
-          _meta {
-            uid
-          }
-          title
-          description
-          image
-          content
-          date
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query ArticleQuery($uid: String) {
+//     allBlog_posts(uid: $uid) {
+//       edges {
+//         node {
+//           _meta {
+//             uid
+//           }
+//           title
+//           description
+//           image
+//           content
+//           date
+//         }
+//       }
+//     }
+//   }
+// `
