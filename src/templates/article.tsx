487f8a131d8fed12f2723ebf41744dc3880fa9ff
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-11-25 12:05:48 +0100

    change function and props in template/article.tsx

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { ReactElement } from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
@@ -6,17 +6,43 @@ import SEO from "../components/seo"
 import Author from "../components/author"
 import CustomRichText from "../components/customRichText"
 import { formatHumanDate } from "../utils/date"
+import { RichText, RichTextBlock } from "prismic-reactjs"
 
-export default function Article(props) {
-  const doc = props.allPrismicBlogPost.slice(0, 1).pop()
+interface PropsArticle {
+  data: {
+    title: {
+      raw: RichTextBlock[]
+    }
+    description: {
+      raw: RichTextBlock[]
+    }
+    content: {
+      text: string
+      raw: RichTextBlock[]
+      html: string
+    }
+    date: string
+    image: {
+      url: string
+      alt: string
+      copyright: string
+      gatsbyImageData
+    }
+  }
+}
 
-  if (!doc) return null
+export default function Article({
+  props,
+}: {
+  props: PropsArticle
+}): ReactElement {
+  console.log(props)
+  // if (!doc) return null
+  const datePublished = formatHumanDate(props.data.date)
+  const title = RichText.asText(props.data.title.raw)
+  const description = RichText.asText(props.data.description.raw)
 
-  const title = doc.node.data.title.text
-  const description = doc.node.data.description.text
-  const datePublished = formatHumanDate(doc.node.data.date)
-
-  const { image } = doc.node.image.url
+  const image = props.data.image.url
 
   return (
     <Layout>
@@ -32,9 +58,15 @@ export default function Article(props) {
         {image && (
           <div className="text-center mb-10">
             <figure className="post-content-figure">
-              <img className="mx-auto" src={image.url} alt={image.alt} />
+              <img
+                className="mx-auto"
+                src={props.data.image.url}
+                alt={props.data.image.alt}
+              />
               <figcaption className="mt-8">
-                {image.alt} {image.copyright && `© ${image.copyright}`}
+                {props.data.image.alt}{" "}
+                {props.data.image.copyright &&
+                  `© ${props.data.image.copyright}`}
               </figcaption>
             </figure>
           </div>
@@ -42,7 +74,8 @@ export default function Article(props) {
 
         <div className="post-content-body">
           <div className="mb-20">
-            <CustomRichText render={doc.node.content.text} />
+            {props.data.content.html}
+            <CustomRichText render={props.data.content.raw} />
           </div>
 
           <p className="text-gray-500 mb-20">
@@ -81,6 +114,8 @@ export const query = graphql`
           }
           content {
             text
+            raw
+            html
           }
           image {
             alt
