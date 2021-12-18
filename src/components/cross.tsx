a0bb0dbbaccc0544a5461388ca4a15370812ab3e
David Leuliette <dleuliette@gmail.com>
2021-12-18 13:35:29 +0100

    Fix ESLint Text error

--- src/components/cross.tsx
+++ src/components/cross.tsx
@@ -10,9 +10,9 @@ function cross() {
       stroke="currentColor"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="M4 6h16M4 12h16M4 18h16"
       />
 
