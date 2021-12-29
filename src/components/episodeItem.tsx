92416a9977bb8f9a54ff1a57c3ee2161e3d9573c
David Leuliette <dleuliette@gmail.com>
2021-12-29 21:26:49 +0100

    hide episode img on small screen

--- src/components/episodeItem.tsx
+++ src/components/episodeItem.tsx
@@ -38,9 +38,9 @@ export default function EpisodeItem({
     <>
       <Link
         to={`/episodes/${episode.guid}`}
-        className="flex flex-col md:flex-row hover:no-underline article-preview  "
+        className="flex flex-col md:flex-row hover:no-underline article-preview"
       >
-        <div className="flex-1 px-6">
+        <div className="flex-1">
           <Text as="h3Link">{episode.title}</Text>
           {withPlayer && (
             <audio controls src={audioSrc} className="mb-8 mt-4" />
@@ -51,7 +51,7 @@ export default function EpisodeItem({
             dangerouslySetInnerHTML={{ __html: summary }}
           />
         </div>
-        <div className="item-episode flex-shrink-0 px-3">
+        <div className="hidden sm:flex flex-shrink-0 px-3">
           <img
             className="w-48 h-48 mt-2"
             src={episode.itunes.image}
