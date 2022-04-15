060b4f0f0227a6b509f0673a91497f981d9d7e54
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-04-15 16:06:48 +0200

    add instagram link closes #114

--- src/components/instagram.tsx
+++ src/components/instagram.tsx
@@ -30,66 +30,100 @@ export default function Instagram() {
       </div>
       <div className="md:show m-auto hidden w-full flex-col justify-center  md:flex md:flex-row lg:w-3/5 lg:justify-around">
         <div className="flex w-1/3 flex-col md:w-1/4">
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          <a
+            href="https://www.instagram.com/p/CXn2XbyoTmT/"
+            target={`_blank`}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/1.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8 "
             />
-          </div>
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          </a>
+          <a
+            href="https://www.instagram.com/p/CXn1qGQoqxx/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/2.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          </a>
+
+          <a
+            href="https://www.instagram.com/p/CXnzlQ0IK8N/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/3.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
+          </a>
         </div>
         <div className="flex w-1/3 flex-col justify-center md:w-1/4">
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          <a
+            href="https://www.instagram.com/p/CNRxx8QIXJ5/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/4.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          </a>
+
+          <a
+            href="https://www.instagram.com/p/CNRxx8QIXJ5/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/5.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
+          </a>
         </div>
         <div className="flex w-1/3 flex-col md:w-1/4">
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          <a
+            href="https://www.instagram.com/p/Cay36_bgT3k/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/6.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          </a>
+          <a
+            href="https://www.instagram.com/p/Cay17dkAKME/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/7.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
-          <div className="m-auto my-4 lg:bg-gray-100 ">
+          </a>
+          <a
+            href="https://www.instagram.com/p/Cay14ynA6Gr/"
+            target={"_blank"}
+            className="m-auto my-4 lg:bg-gray-100 "
+          >
             <StaticImage
               src="../images/instagram/8.jpg"
               alt="Logo podcast ART au feminin"
               className="m-0 my-2 p-0 md:p-20 lg:m-8"
             />
-          </div>
+          </a>
         </div>
       </div>
     </div>
