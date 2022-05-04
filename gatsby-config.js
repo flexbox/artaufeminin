ee22ee57e19281c21ffb252dd08edc1d45d7a7ad
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2022-05-04 13:41:10 +0200

    fix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-sitemap",
       options: {
-        output: `/sitemap`,
+        output: `/`,
         query: `
         {
           allSitePage {
