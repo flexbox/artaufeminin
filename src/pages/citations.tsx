d83ea1bf04e8907dac40cd2ccd3ed298c7c1804a
David Leuliette <dleuliette@gmail.com>
2023-06-09 14:30:49 +0200

    test: 💍 import import QuotationProps from quotation.d.ts
    ✅ Closes: #144

--- src/pages/citations.tsx
+++ src/pages/citations.tsx
@@ -4,16 +4,13 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import QuotationList from "../components/quotationListItem"
 import LayoutSidebar from "../components/layoutSidebar"
+import { QuotationProps } from "../components/quotation" // Importing QuotationProps from quotation.d.ts
 
-interface QuotationPageProps {
-  data: {
-    allPrismicQuotation: {
-      nodes: [] // it should be something like PrismicBlogPost[] insteead of a simple []
-    }
-  }
-}
-
-const QuotationPage = ({ data }: QuotationPageProps) => {
+const QuotationPage = ({
+  data,
+}: {
+  data: { allPrismicQuotation: { nodes: QuotationProps[] } }
+}) => {
   const quotation = data.allPrismicQuotation.nodes
 
   return (
