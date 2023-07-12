13129cdc9dc2f3dc6b1cbc2f1a6194fced07d90c
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-07-12 14:45:31 +0200

    feat: 🎸 Add podcast player for last epid sode in layout
    ✅ Closes: #145

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react"
+import React, { ReactNode, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Footer from "./footer"
 import Header from "./header"
@@ -12,6 +12,13 @@ interface LayoutProps {
 function Layout({ children, withInstagram = false }: LayoutProps) {
   const data = useStaticQuery(graphql`
     query {
+      allAnchorEpisode {
+        nodes {
+          enclosure {
+            url
+          }
+        }
+      }
       site {
         siteMetadata {
           title
@@ -19,7 +26,21 @@ function Layout({ children, withInstagram = false }: LayoutProps) {
       }
     }
   `)
+  const audioSrc = data.allAnchorEpisode.nodes[0].enclosure.url
+  console.log("🚀 ~ file: layout.tsx:25 ~ Layout ~ audioSrc:", audioSrc)
   const siteTitle = data.site.siteMetadata.title
+
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const togglePlay = () => {
+    const audioElement = document.getElementById("audio-element")
+    if (isPlaying) {
+      audioElement?.pause()
+    } else {
+      audioElement?.play()
+    }
+    setIsPlaying(!isPlaying)
+  }
 
   return (
     <div className="bg-gray-50">
@@ -28,6 +49,13 @@ function Layout({ children, withInstagram = false }: LayoutProps) {
         {children}
       </main>
       {withInstagram && <Instagram />}
+      <div className="z-1000 fixed bottom-0 left-0 m-auto flex w-full items-center bg-[#f2f2f2] p-4">
+        <button
+          className={`play-pause-button ${isPlaying ? "playing" : ""}`}
+          onClick={togglePlay}
+        />
+        <audio controls src={audioSrc} className="w-full" />
+      </div>
       <Footer siteTitle={siteTitle} />
     </div>
   )
