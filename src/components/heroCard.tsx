f1241a28e36de25bb97d5395fffb288352b7ae6a
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-09-14 00:51:41 +0200

    feat: 🎸 add cva for heroCard

---
+++ src/components/heroCard.tsx
@@ -0,0 +1,86 @@
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid"
+import { type VariantProps, cva } from "class-variance-authority"
+import { Link } from "gatsby"
+import React from "react"
+
+import Hero from "./hero"
+import Text from "./text"
+
+export const heroCardVariants = cva(["primary"], {
+  variants: {
+    size: {
+      md: ["bg-cover", "bg-center", "h-52", "md:h-1/2", "overflow-hidden"],
+      lg: ["flex-1", "mb-4", "md:mb-0", "h-52", "md:h-full", "overflow-hidden"],
+    },
+    text: {
+      primary: [
+        "text-white",
+        "bg-black",
+        "items-end",
+        "flex",
+        "mb-6",
+        "mx-auto",
+        "p-2",
+        "w-5/6",
+        "text-left",
+      ],
+    },
+    image: {
+      primary: [
+        "bg-cover",
+        "bg-center",
+        "h-full",
+        "duration-300",
+        "flex",
+        "items-end",
+        "bg-no-repeat",
+        "hover:scale-105",
+        "overflow-hidden",
+      ],
+    },
+  },
+
+  defaultVariants: {
+    text: "primary",
+    image: "primary",
+    size: "lg",
+  },
+})
+
+export interface HeroCardProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof heroCardVariants> {
+  heroLink: string
+  imageUrl: string
+  heroTitle: string
+}
+
+export const HeroCard: React.FC<HeroCardProps> = ({
+  className,
+  text,
+  image,
+  imageUrl,
+  size,
+  heroLink,
+  heroTitle,
+  ...rest
+}) => {
+  return (
+    <div className={heroCardVariants({ className, size })}>
+      <Link to={heroLink}>
+        <div
+          className={heroCardVariants({ image })}
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        >
+          <Text as="h3" className={heroCardVariants({ text })}>
+            {heroTitle}
+          </Text>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default HeroCard
