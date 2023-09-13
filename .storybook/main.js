5ea3182079772c2b96ecd9012806b1bcb627cee3
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-09-13 21:11:02 +0200

    fix: 🐛 Try to fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,39 @@
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-links",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        postCss: {
-          implementation: require("postcss"),
-        },
-      },
-    },
-  ],
-  framework: {
-    name: "@storybook/react-webpack5",
-    options: {},
+  // You will want to change this to wherever your Stories will live
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
   },
-  docs: {
-    autodocs: true,
+  webpackFinal: async (config) => {
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+
+    // Use installed babel-loader which is v8.0-beta (which is meant to work with @babel/core@7)
+    config.module.rules[0].use[0].loader = require.resolve("babel-loader")
+
+    // Use @babel/preset-react for JSX and env (instead of staged presets)
+    config.module.rules[0].use[0].options.presets = [
+      require.resolve("@babel/preset-react"),
+      require.resolve("@babel/preset-env"),
+    ]
+
+    config.module.rules[0].use[0].options.plugins = [
+      // Use @babel/plugin-proposal-class-properties for class arrow functions
+      require.resolve("@babel/plugin-proposal-class-properties"),
+
+      // Use babel-plugin-remove-graphql-queries to remove graphql queries from components when rendering in Storybook
+      // While still rendering content from useStaticQuery in development mode
+      [
+        require.resolve("babel-plugin-remove-graphql-queries"),
+        {
+          stage: config.mode === `development` ? "develop-html" : "build-html",
+          staticQueryDir: "page-data/sq/d",
+        },
+      ],
+    ]
+
+    return config
   },
 }
--- src/components/article-list.stories.tsx
+++ src/components/article-list.stories.tsx
@@ -1,41 +1,65 @@
-import { storiesOf } from "@storybook/react"
+import { Meta, StoryFn, storiesOf } from "@storybook/react"
 import React from "react"
 
-import { ArticleList } from "./article-list"
+import { ArticleList, ArticleProps } from "./article-list"
 
-const allArticles = [
+export default {
+  title: "Example/ArticleList",
+  component: ArticleList,
+} as Meta<typeof ArticleList>
+
+const allArticles: ArticleProps[] = [
   {
-    uid: "example-article-1",
+    uid: "article1",
     data: {
       title: {
-        richText: [{ text: "Example Article 1" }],
+        richText: [{ type: "paragraph", text: "Article 1 Title" }],
       },
       description: {
-        richText: [{ text: "This is an example article." }],
+        richText: [{ type: "paragraph", text: "Article 1 Description" }],
       },
-      date: "2021-01-01",
+      date: "2023-09-13",
       image: {
-        url: "https://example.com/image.jpg",
+        url: "article1.jpg",
       },
     },
   },
   {
-    uid: "example-article-2",
+    uid: "article2",
     data: {
       title: {
-        richText: [{ text: "Example Article 2" }],
+        richText: [{ type: "paragraph", text: "Article 2 Title" }],
       },
       description: {
-        richText: [{ text: "This is another example article." }],
+        richText: [{ type: "paragraph", text: "Article 2 Description" }],
       },
-      date: "2021-01-02",
+      date: "2023-09-14",
       image: {
-        url: "https://example.com/image2.jpg",
+        url: "article2.jpg",
+      },
+    },
+  },
+  {
+    uid: "article3",
+    data: {
+      title: {
+        richText: [{ type: "paragraph", text: "Article 3 Title" }],
+      },
+      description: {
+        richText: [{ type: "paragraph", text: "Article 3 Description" }],
+      },
+      date: "2023-09-15",
+      image: {
+        url: "article3.jpg",
       },
     },
   },
 ]
 
-storiesOf("ArticleList", module).add("default", () => (
-  <ArticleList allArticles={allArticles} />
-))
+const Template: StoryFn<typeof ArticleList> = (args) => {
+  return <ArticleList allArticles={allArticles} />
+}
+
+export const Primary = {
+  render: Template,
+}
