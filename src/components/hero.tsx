265aefcaa77551ac290e1b97d2f082a2f0947111
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-09-14 00:02:12 +0200

    fix: 🐛 overflow and text for hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,12 +10,12 @@ type Props = {
 
 export default function Hero({ allEpisodes }: Props) {
   const textStyle =
-    "text-white bg-black items-end flex justify-end p-2 my-2 mx-auto"
+    "text-white bg-black items-end flex  mb-6 mx-auto p-2 w-5/6 text-left"
   const imgStyle =
-    "bg-cover bg-center h-full ease-in-out transition-all duration-300 hover:opacity-95 flex items-end hover:scale-95"
+    "bg-cover bg-center h-full ease-in-out transition-all duration-300 flex items-end bg-no-repeat hover:scale-105 overflow-hidden "
   return (
     <div className="h-screen my-auto -mb-32 md:-mb-48 w-3/4 m-auto">
-      <div className="flex-col md:flex md:flex-row gap-4 h-2/3">
+      <div className="flex-col md:flex md:flex-row gap-4 h-2/3 overflow-hidden">
         <div className="flex-1 mb-4 md:mb-0 h-52 md:h-full">
           <Link to={`/podcast/${allEpisodes[0].guid}`}>
             <div
@@ -30,7 +30,7 @@ export default function Hero({ allEpisodes }: Props) {
             </div>
           </Link>
         </div>
-        <div className="flex-1 flex flex-col gap-4">
+        <div className="flex-1 flex flex-col gap-4 overflow-hidden">
           <div className="bg-cover bg-center h-52 md:h-1/2 ">
             <Link to={`/podcast/${allEpisodes[1].guid}`}>
               <div
@@ -46,7 +46,7 @@ export default function Hero({ allEpisodes }: Props) {
             </Link>
           </div>
 
-          <div className="bg-cover bg-center h-52 md:h-1/2">
+          <div className="bg-cover bg-center h-52 md:h-1/2 overflow-hidden">
             <Link to={`/podcast/${allEpisodes[2].guid}`}>
               <div
                 className={imgStyle}
