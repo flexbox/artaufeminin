c23c8fe3a88901775b138b2caa57a897b74bf405
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-09-14 11:52:00 +0200

    fix: 🐛 update cva for heroCard

--- src/components/heroCard.tsx
+++ src/components/heroCard.tsx
@@ -3,7 +3,6 @@ import { type VariantProps, cva } from "class-variance-authority"
 import { Link } from "gatsby"
 import React from "react"
 
-import Hero from "./hero"
 import Text from "./text"
 
 export const heroCardVariants = cva(["primary"], {
@@ -12,37 +11,9 @@ export const heroCardVariants = cva(["primary"], {
       md: ["bg-cover", "bg-center", "h-52", "md:h-1/2", "overflow-hidden"],
       lg: ["flex-1", "mb-4", "md:mb-0", "h-52", "md:h-full", "overflow-hidden"],
     },
-    text: {
-      primary: [
-        "text-white",
-        "bg-black",
-        "items-end",
-        "flex",
-        "mb-6",
-        "mx-auto",
-        "p-2",
-        "w-5/6",
-        "text-left",
-      ],
-    },
-    image: {
-      primary: [
-        "bg-cover",
-        "bg-center",
-        "h-full",
-        "duration-300",
-        "flex",
-        "items-end",
-        "bg-no-repeat",
-        "hover:scale-105",
-        "overflow-hidden",
-      ],
-    },
   },
 
   defaultVariants: {
-    text: "primary",
-    image: "primary",
     size: "lg",
   },
 })
@@ -57,24 +28,26 @@ export interface HeroCardProps
 
 export const HeroCard: React.FC<HeroCardProps> = ({
   className,
-  text,
-  image,
   imageUrl,
   size,
   heroLink,
   heroTitle,
   ...rest
 }) => {
+  const textStyle =
+    "text-white bg-black items-end flex  mb-6 mx-auto p-2 w-5/6 text-left"
+  const imgStyle =
+    "bg-cover bg-center h-full ease-in-out transition-all duration-300 flex items-end bg-no-repeat hover:scale-105 overflow-hidden"
   return (
     <div className={heroCardVariants({ className, size })}>
       <Link to={heroLink}>
         <div
-          className={heroCardVariants({ image })}
+          className={imgStyle}
           style={{
             backgroundImage: `url(${imageUrl})`,
           }}
         >
-          <Text as="h3" className={heroCardVariants({ text })}>
+          <Text as="h3" className={textStyle}>
             {heroTitle}
           </Text>
         </div>
