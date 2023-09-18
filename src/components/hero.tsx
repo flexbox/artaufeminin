e4ffc92bbe65501efed788dfb08ed4d055db0c7d
David Leuliette <dleuliette@gmail.com>
2023-09-18 10:50:59 +0200

    fix: 🐛 better slug for SEO active page

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -14,20 +14,20 @@ export default function Hero({ allEpisodes }: Props) {
     <div className="h-screen my-auto -mb-32 md:-mb-48 w-3/4 m-auto">
       <div className="flex-col md:flex md:flex-row gap-4 h-2/3 overflow-hidden">
         <HeroCard
-          heroLink={`/podcast/${allEpisodes[0].guid}`}
+          heroLink={`/podcasts/${allEpisodes[0].guid}`}
           imageUrl={allEpisodes[0].itunes.image}
           heroTitle={allEpisodes[0].title}
           size={"lg"}
         />
         <div className="flex-1 flex flex-col gap-4 overflow-hidden">
           <HeroCard
-            heroLink={`/podcast/${allEpisodes[1].guid}`}
+            heroLink={`/podcasts/${allEpisodes[1].guid}`}
             imageUrl={allEpisodes[1].itunes.image}
             heroTitle={allEpisodes[1].title}
             size={"md"}
           />
           <HeroCard
-            heroLink={`/podcast/${allEpisodes[2].guid}`}
+            heroLink={`/podcasts/${allEpisodes[2].guid}`}
             imageUrl={allEpisodes[2].itunes.image}
             heroTitle={allEpisodes[2].title}
             size={"md"}
