0d976276965b5bb7df93e2f6bcaa8a1f692f8b4a
David Leuliette <dleuliette@gmail.com>
2024-01-30 09:08:35 +0100

    feat: 🎸 order by date for quote

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -3,19 +3,18 @@ import React from "react"
 import { QuotationProps } from "./quotation"
 
 export function Quote({ data }: QuotationProps) {
-  const author = data.author
-  const quote = data.quotation
+  const { author, quotation } = data
 
   return (
-    <blockquote className="quote relative mb-4 mt-4 p-4 text-xl italic text-gray-600">
+    <blockquote className="quote relative mb-32 mt-12 p-4 text-2xl italic text-gray-600">
       <div className="stylistic-quote-mark" aria-hidden="true">
         &ldquo;
       </div>
-      <p className="mb-4">{quote}</p>
+      <p className="mb-4">{quotation}</p>
       {author && (
         <cite className="flex items-center">
           <div className="flex flex-col items-start">
-            <span className="mb-1 text-sm font-bold italic">— {author}</span>
+            <span className="text-md font-bold italic">— {author}</span>
           </div>
         </cite>
       )}
