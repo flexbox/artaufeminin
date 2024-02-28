ec5d5c92203ba99124171f1147f465215c924c0b
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-02-28 13:44:31 +0100

    fix: 🐛 order of recent articles

--- src/components/articlesHero.tsx
+++ src/components/articlesHero.tsx
@@ -13,6 +13,12 @@ export function ArticlesHero({ allArticles }: ArticlesHeroProps) {
   return (
     <div className="m-auto -mb-32 h-screen w-3/4 md:-mb-48">
       <div className="h-2/3 flex-col gap-4 overflow-hidden md:flex md:flex-row">
+        <HeroCard
+          heroLink={`/articles/${allArticles[0].uid}`}
+          imageUrl={allArticles[0].data.image.url}
+          heroTitle={RichText.asText(allArticles[0].data.title.richText)}
+          size={'lg'}
+        />
         <div className="flex flex-1 flex-col gap-4 overflow-hidden">
           <HeroCard
             heroLink={`/articles/${allArticles[1].uid}`}
@@ -27,14 +33,8 @@ export function ArticlesHero({ allArticles }: ArticlesHeroProps) {
             size={'md'}
           />
         </div>
-        <HeroCard
-          heroLink={`/articles/${allArticles[0].uid}`}
-          imageUrl={allArticles[0].data.image.url}
-          heroTitle={RichText.asText(allArticles[0].data.title.richText)}
-          size={'lg'}
-        />
       </div>
-      <div className=" mt-12 flex justify-end md:mt-8">
+      <div className="mt-12 flex justify-end md:mt-8">
         <Link to={'/articles'}>
           <Button variant="outline" size="s">
             Lire tous les articles
